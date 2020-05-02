ae4518657f09748cbea5664c84edbdc26cb99e09
Soul <sornwilliam@gmail.com>
2020-05-02 18:44:53 -0500

    finally got local storage going on script.js. for some reason after adding a new event. older events are turned into empty strings and not saved anymore.

--- Develop/script.js
+++ Develop/script.js
@@ -1,1 +1,30 @@
-var blocks = $("#container");
+var savebtn = $(".saveBtn");
+var description = $(".description");
+
+console.log(localStorage);
+
+savebtn.on("click", function(event) {
+    event.preventDefault()
+    for(let i = 1; i< 11; i++){
+
+        
+
+        var saved = $(".block"+[i]).val();
+
+        console.log(saved);
+
+        localStorage.setItem("events"+[i], saved);
+
+    }
+});
+
+
+
+function renderEvents(){
+    for(let i = 1; i < 11; i++){
+        var events = localStorage.getItem("events"+[i]);
+        
+        $(".block"+[i]).text = events;
+    }
+    console.log(events);
+};

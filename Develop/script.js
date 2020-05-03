d93d0fcbb6c02ba3558ca5dba4af47ca5e738bec
Soul <sornwilliam@gmail.com>
2020-05-02 20:38:25 -0500

    added 2 example.pngs. fiddled around with jquery for color change. ended up making everything grey.

--- Develop/script.js
+++ Develop/script.js
@@ -38,10 +38,41 @@ function colorChange(){
         }
     })
 }
-
-    function colorChange2(){
-
-    }
+    //trying to make my own version of above w/jquery
+    // NOTE ** it goes through but makes everything past for some reason.
+    // function colorChange2(){
+    //     Array.from(rows).forEach(row => {
+    //         let
+    //             rowIdString = row.id,
+    //             rowHour;
+    //         if (rowIdString) {
+    //             rowHour = parseInt(rowIdString);
+    //         }
+    //         if(rowHour){
+    //             if(currentHour === rowHour){
+    //                 rows.addClass("present");
+    //                 rows.removeClass("future");
+    //                 rows.removeClass("past");
+    //                 rows.removeClass("hour");
+    //             }else if ((currentHour < rowHour) && (currentHour > rowHour -6)){
+    //                 rows.addClass("future");
+    //                 rows.removeClass("present");
+    //                 rows.removeClass("past");
+    //                 rows.removeClass("hour");
+    //             }else if ((currentHour > rowHour) && (currentHour < rowHour +6)){
+    //                 rows.addClass("past");
+    //                 rows.removeClass("future");
+    //                 rows.removeClass("present");
+    //                 rows.removeClass("hour");
+    //             }else{
+    //                 rows.addClass("hour");
+    //                 rows.removeClass("future");
+    //                 rows.removeClass("past");
+    //                 rows.removeClass("present");
+    //             }
+    //         }
+    //     })
+    // }
 
 function setColor(element, color) {
     element.style.backgroundColor = color;
